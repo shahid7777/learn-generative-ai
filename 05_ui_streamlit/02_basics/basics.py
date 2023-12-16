14691c154d113be6e4ced4bea980023cf4b9cb28
Sourcery AI <>
2023-12-16 02:01:03 +0000

    'Refactored by Sourcery'

--- 05_ui_streamlit/02_basics/basics.py
+++ 05_ui_streamlit/02_basics/basics.py
@@ -1,12 +1,12 @@
 # import module
 import streamlit as st
- 
+
 # Title
 st.title("This is a title")
 
 # Header
 st.header("This is a header") 
- 
+
 # Subheader
 st.subheader("This is a subheader")
 
@@ -18,32 +18,32 @@ st.markdown("### This is a markdown")
 
 # success
 st.success("Success")
- 
+
 # success
 st.info("Information")
- 
+
 # success
 st.warning("Warning")
- 
+
 # success
 st.error("Error")
- 
+
 # Exception - This has been added later
 exp = ZeroDivisionError("Trying to divide by Zero")
 st.exception(exp)
 
 # Write text
 st.write("Text with write")
- 
+
 # Writing python inbuilt function range()
 st.write(range(10))
 
 # Display Images
- 
+
 # import Image from pillow to open images
 from PIL import Image
 img = Image.open("streamlit.png")
- 
+
 # display image using streamlit
 # width is used to set the width of an image
 st.image(img, width=200)
@@ -60,7 +60,7 @@ if st.checkbox("Show/Hide"):
 # first argument is the title of the radio button
 # second argument is the options for the radio button
 status = st.radio("Select Gender: ", ('Male', 'Female'))
- 
+
 
 # conditional statement to print 
 # Male if male is selected else print female
@@ -73,24 +73,24 @@ else:
 
 
 # Selection box
- 
+
 # first argument takes the title of the selectionbox
 # second argument takes options
 hobby = st.selectbox("Hobbies: ",
                      ['Dancing', 'Reading', 'Sports'])
- 
+
 # print the selected hobby
 st.write("Your hobby is: ", hobby)
 
 
 
 # multi select box
- 
+
 # first argument takes the box title
 # second argument takes the options to show
 hobbies = st.multiselect("Hobbies: ",
                          ['Dancing', 'Reading', 'Sports'])
- 
+
 # write the selected options
 st.write("You selected", len(hobbies), 'hobbies')
 
@@ -99,7 +99,7 @@ st.write("You selected", len(hobbies), 'hobbies')
 
 # Create a simple button that does nothing
 st.button("Click me for no reason")
- 
+
 
 # Create a button, that when clicked, shows a text
 if(st.button("About")):
@@ -108,12 +108,12 @@ if(st.button("About")):
 
 
 # Text Input
- 
+
 # save the input text in the variable 'name'
 # first argument shows the title of the text input box
 # second argument displays a default text inside the text input area
 name = st.text_input("Enter Your name", "Type Here ...")
- 
+
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
 if(st.button('Submit')):
@@ -124,16 +124,16 @@ if(st.button('Submit')):
 
 
 # slider
- 
+
 # first argument takes the title of the slider
 # second argument takes the starting of the slider
 # last argument takes the end number
 level = st.slider("Select the level", 1, 5)
- 
+
 # print the level
 # format() is used to print value 
 # of a variable at a specific position
-st.text('Selected: {}'.format(level))
+st.text(f'Selected: {level}')
 
 
 
